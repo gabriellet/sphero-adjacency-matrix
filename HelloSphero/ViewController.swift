68be286c539f9575648d2a89348769e711b78a3b
Gabrielle <gat2118@columbia.edu>
2016-09-30 02:09:17 -0400

    Sphero drive app UI almost completed.

--- HelloSphero/ViewController.swift
+++ HelloSphero/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var step6: UILabel!
     @IBOutlet weak var step7: UILabel!
     @IBOutlet weak var step8: UILabel!
+    var leftStep: Double = 0.0
+    var rightStep: Double = 0.0
+    var forwardStep: Double = 0.0
+    var backwardStep: Double = 0.0
     
     
     override func viewDidLoad() {
@@ -29,22 +33,30 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPress(_ sender: UIButton) {
         switch (sender.tag) {
-            case 0: responseText.text = "Greetings mortals."
-            case 1: responseText.text = "Hello World."
-            case 2: responseText.text = "Goodbye cruel master."
-            case 3: responseText.text = "2 is the magic number"
+            case 0: responseText.text = "Drive left \(leftStep)"
+            case 1: responseText.text = "Drive right \(rightStep)"
+            case 2: responseText.text = "Drive forward \(forwardStep)"
+            case 3: responseText.text = "Drive backward \(backwardStep)"
             default: responseText.text = "-----"
         }
         //responseText.text = "Greetings mortals."
         
     }
 
+    @IBAction func sayHello(_ sender: UIButton) {
+        responseText.text = "Hello World!"
+    }
+    
     @IBAction func stepperPress(_ sender: UIStepper) {
         switch (sender.tag) {
         case 5: step5.text = String(sender.value)
+            leftStep = sender.value
         case 6: step6.text = String(sender.value)
+            rightStep = sender.value
         case 7: step7.text = String(sender.value)
+            forwardStep = sender.value
         case 8: step8.text = String(sender.value)
+            backwardStep = sender.value
         default: break
         }
     }
