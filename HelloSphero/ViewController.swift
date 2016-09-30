11545b1efee84f1ff7e2b03fc993570be728f70e
Gabrielle <gat2118@columbia.edu>
2016-09-30 01:57:33 -0400

    Working UI

--- HelloSphero/ViewController.swift
+++ HelloSphero/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var responseText: UILabel!
+    @IBOutlet weak var step5: UILabel!
+    @IBOutlet weak var step6: UILabel!
+    @IBOutlet weak var step7: UILabel!
+    @IBOutlet weak var step8: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPress(_ sender: UIButton) {
+        switch (sender.tag) {
+            case 0: responseText.text = "Greetings mortals."
+            case 1: responseText.text = "Hello World."
+            case 2: responseText.text = "Goodbye cruel master."
+            case 3: responseText.text = "2 is the magic number"
+            default: responseText.text = "-----"
+        }
+        //responseText.text = "Greetings mortals."
+        
+    }
 
+    @IBAction func stepperPress(_ sender: UIStepper) {
+        switch (sender.tag) {
+        case 5: step5.text = String(sender.value)
+        case 6: step6.text = String(sender.value)
+        case 7: step7.text = String(sender.value)
+        case 8: step8.text = String(sender.value)
+        default: break
+        }
+    }
+   
 }
 
