f77ca5eec7e837e559cb116ac8f74d149d033758
Gabrielle <gat2118@columbia.edu>
2016-11-10 15:44:36 -0500

    Sphero continues to drive on button press and hold. Can display tail light. Handled case where Sphero is already connected when app starts, no longer causes crash. Button styling change. Increased async message rate for higher collision location accuracy.

--- Adjacency/ViewController.swift
+++ Adjacency/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController, RKResponseObserver {
     @IBOutlet weak var speedLabel: UILabel!
     
     @IBOutlet weak var gridView: GridView!
+    @IBOutlet weak var showTail: UIButton!
+    @IBOutlet weak var sleepButton: UIButton!
+    @IBOutlet var directionButtons: [UIButton]!
     
     var ledON = false
 
@@ -51,21 +54,37 @@ class ViewController: UIViewController, RKResponseObserver {
     var arraySize = 1000
     var adjMapVal: Int = 1000 / 2
     
+    var driveTimer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // possibly do clean up if open connection exists i.e. from force quit
         //RKRobotDiscoveryAgent.disconnectAll()
         //stopDiscovery()
+        NSLog("viewDidLoad")
+        //NSLog("starting discovery")
+        //startDiscovery()
+        //RKRobotDiscoveryAgent.shared().addNotificationObserver(self, selector: #selector(handleRobotStateChangeNotification))
+        showTail.layer.cornerRadius = 10
+        sleepButton.layer.cornerRadius = 10
+        for button in directionButtons {
+            button.layer.cornerRadius = 5
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        NSLog("viewWillAppear")
+        RKRobotDiscoveryAgent.disconnectAll()
+        //NSLog("stop discovery")
+        //stopDiscovery()
+        NSLog("start discovery")
         startDiscovery()
         RKRobotDiscoveryAgent.shared().addNotificationObserver(self, selector: #selector(handleRobotStateChangeNotification))
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        RKRobotDiscoveryAgent.disconnectAll()
-        stopDiscovery()
-        startDiscovery()
-        RKRobotDiscoveryAgent.shared().addNotificationObserver(self, selector: #selector(handleRobotStateChangeNotification))
+    override func viewDidAppear(_ animated: Bool) {
+        NSLog("viewDidAppear")
     }
 
     override func didReceiveMemoryWarning() {
@@ -83,64 +102,60 @@ class ViewController: UIViewController, RKResponseObserver {
             robot.sleep()
         }
     }
-
-    @IBAction func buttonPress(_ sender: UIButton) {
-        switch (sender.tag) {
-            case 0: responseText.text = "Drive North"
-                directionControl(dir: .North)
-            case 1: responseText.text = "Drive North East"
-                directionControl(dir: .NorthEast)
-            case 2: responseText.text = "Drive East"
-                directionControl(dir: .East)
-            case 3: responseText.text = "Drive South East"
-                directionControl(dir: .SouthEast)
-            case 4: responseText.text = "Drive South"
-                directionControl(dir: .South)
-            case 5: responseText.text = "Drive South West"
-                directionControl(dir: .SouthWest)
-            case 6: responseText.text = "Drive West"
-                directionControl(dir: .West)
-            case 7: responseText.text = "Drive North West"
-                directionControl(dir: .NorthWest)
-            default: responseText.text = "-----"
-        }
-        
-    }
     
-    // TODO: make press and hold action work
-    /*
-    var timer: Timer = Timer.init()
-    
-    func driveButtonHold() {
-        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(driveButtonHold), userInfo: nil, repeats: true)
-    }
-    
-    func driveButtonStop() {
-        
-    }
-    
-    @IBAction func buttonHold(_ sender: UIButton) {
-        switch (sender.tag) {
-        case 0: responseText.text = "Drive North"
-        directionControl(dir: .North)
-        case 1: responseText.text = "Drive North East"
-        directionControl(dir: .NorthEast)
-        case 2: responseText.text = "Drive East"
-        directionControl(dir: .East)
-        case 3: responseText.text = "Drive South East"
-        directionControl(dir: .SouthEast)
-        case 4: responseText.text = "Drive South"
-        directionControl(dir: .South)
-        case 5: responseText.text = "Drive South West"
-        directionControl(dir: .SouthWest)
-        case 6: responseText.text = "Drive West"
-        directionControl(dir: .West)
-        case 7: responseText.text = "Drive North West"
-        directionControl(dir: .NorthWest)
-        default: responseText.text = "-----"
-        }
+    /* @IBAction func clearGrid(_ sender: UIButton) {
+        occupancy = [[Int]](repeating:[Int](repeating:0, count:1000), count:1000)
+        gridView.setNeedsDisplay()
     } */
     
+    @IBAction func showTail(_ sender: UIButton) {
+        if let robot = self.robot {
+            robot.setLEDWithRed(0.0, green: 0.0, blue: 0.0)
+            robot.setBackLEDBrightness(1.0)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+            self.tailOff()
+        }
+    }
+    
+    func tailOff() {
+        if let robot = self.robot {
+            robot.setBackLEDBrightness(0.0)
+            robot.setLEDWithRed(0.1, green: 0.8, blue: 0.2)
+        }
+    }
+    
+    @IBAction func driveButtonDown(_ sender: UIButton) {
+        var dir: DriveDir = .North
+        switch (sender.tag) {
+            case 0: responseText.text = "Drive North"
+                dir = .North
+            case 1: responseText.text = "Drive North East"
+                dir = .NorthEast
+            case 2: responseText.text = "Drive East"
+                dir = .East
+            case 3: responseText.text = "Drive South East"
+                dir = .SouthEast
+            case 4: responseText.text = "Drive South"
+                dir = .South
+            case 5: responseText.text = "Drive South West"
+                dir = .SouthWest
+            case 6: responseText.text = "Drive West"
+                dir = .West
+            case 7: responseText.text = "Drive North West"
+                dir = .NorthWest
+            default: responseText.text = "-----"
+        }
+        directionControl(dir: dir)
+        driveTimer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(repeatDirectionControl), userInfo: dir, repeats: true)
+    }
+    
+    @IBAction func driveButtonUp(_ sender: UIButton) {
+        if let robot = self.robot {
+            robot.stop()
+        }
+        driveTimer.invalidate()
+    }
     
     
     @IBAction func stepperPress(_ sender: UIStepper) {
@@ -159,20 +174,6 @@ class ViewController: UIViewController, RKResponseObserver {
         RKRobotDiscoveryAgent.stopDiscovery()
     }
     
-    func toggleLED() {
-        if let robot = self.robot {
-            if (ledON) {
-                robot.setLEDWithRed(0.0, green: 0.0, blue: 0.0)
-            } else {
-                robot.setLEDWithRed(0.0, green: 0.8, blue: 0.0)
-            }
-            ledON = !ledON
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                self.toggleLED()
-            }
-        }
-    }
-    
     func directionControl(dir: DriveDir) {
         //NSLog("I believe I can drive")
         switch dir {
@@ -189,13 +190,28 @@ class ViewController: UIViewController, RKResponseObserver {
         drive()
     }
     
+    func repeatDirectionControl(driveTimer: Timer) {
+        //NSLog("I believe I can drive")
+        switch driveTimer.userInfo as! DriveDir {
+        case .North: hdg = 0.0
+        case .NorthEast: hdg = 45.0
+        case .East: hdg = 90.0
+        case .SouthEast: hdg = 135.0
+        case .South: hdg = 180.0
+        case .SouthWest: hdg = 225.0
+        case .West: hdg = 270.0
+        case .NorthWest: hdg = 315.0
+        }
+        dst = Float(speed)
+        drive()
+        // directionControl(dir: driveTimer.userInfo as! DriveDir)
+    }
+    
     func drive() {
         if let robot = self.robot {
-            robot.setLEDWithRed(0.7, green: 0.0, blue: 0.2)
+            robot.setLEDWithRed(0.1, green: 0.8, blue: 0.2)
             //NSLog("heading: \(hdg), distance: \(dst)")
             robot.drive(withHeading: hdg, andVelocity: dst)
-            //self.robot.send(RKSetHeadingCommand(heading: 0.0))
-            //self.robot.send(RKRollCommand(heading: hdg, velocity: 1.0, andDistance: dst))
             //NSLog("One small step for Gabrielle, one large roll for me.")
         }
     }
@@ -203,12 +219,14 @@ class ViewController: UIViewController, RKResponseObserver {
     func handle(_ message: RKAsyncMessage!, forRobot robot: RKRobotBase!) {
         //NSLog("Async Message")
         if let sensorMessage = message as? RKDeviceSensorsAsyncData {
-            let sensorData = sensorMessage.dataFrames.last as? RKDeviceSensorsData
-            let locator = sensorData?.locatorData
-            locatorPositionX = locator!.position.x
-            locatorPositionY = locator!.position.y
-            positionLabel.text = "(\(locatorPositionX), \(locatorPositionY))"
-            //NSLog("Current Position:(\(locatorPositionX), \(locatorPositionY))")
+            if let sensorData = sensorMessage.dataFrames.last as? RKDeviceSensorsData {
+                if let locator = sensorData.locatorData {
+                    locatorPositionX = locator.position.x
+                    locatorPositionY = locator.position.y
+                    positionLabel.text = "(\(locatorPositionX), \(locatorPositionY))"
+                    //NSLog("Current Position:(\(locatorPositionX), \(locatorPositionY))")
+                }
+            }
             
         }
         else if let sensorMessage = message as? RKCollisionDetectedAsyncData {
@@ -216,11 +234,9 @@ class ViewController: UIViewController, RKResponseObserver {
             //get data from sphero on collision
             collisionTime = sensorMessage.impactTimeStamp
             collisionSpeed = sensorMessage.impactSpeed
-            collisionAxisX = sensorMessage.impactAxis.x
-            collisionAxisY = sensorMessage.impactAxis.y
             
             //change sphero color on collision, print message to app
-            self.robot.setLEDWithRed(0.1, green: 0.8, blue: 0.2)
+            self.robot.setLEDWithRed(0.7, green: 0.0, blue: 0.2)
             responseText.text = "OUCH!"
             gridView.setNeedsDisplay()
             
@@ -245,37 +261,43 @@ class ViewController: UIViewController, RKResponseObserver {
     }
     
     func handleRobotStateChangeNotification(notification: RKRobotChangedStateNotification) {
-        guard let noteRobot = notification.robot else {return}
-        
-        switch (notification.type) {
-        case .connecting:
-            connectionLabel.text = "\(notification.robot.name()) Connecting"
-            break
-        case .online:
-            guard let conveniencerobot = RKConvenienceRobot(robot: noteRobot) else {return}
-            if (UIApplication.shared.applicationState != .active) {
-                conveniencerobot.disconnect()
-            } else {
-                robot = RKConvenienceRobot(robot: noteRobot)
-                connectionLabel.text = noteRobot.name()
-                responseText.text = "Greetings mortals."
-                robot.add(self) //get responses from sphero
-                robot.enableStabilization(true)
-                robot.enableCollisions(true)
-                robot.enableLocator(true)
-                let sensorMask = RKDataStreamingMask.locatorAll
-                robot.enableSensors(sensorMask, at: RKStreamingRate.dataStreamingRate10)
-                robot.setZeroHeading() //current position is (0,0)
+        if let noteRobot = notification.robot {
+            switch (notification.type) {
+            case .connecting:
+                NSLog("Connecting Sphero")
+                connectionLabel.text = "\(notification.robot.name()) Connecting"
+                break
+            case .online:
+                NSLog("Sphero Online")
+                if let conveniencerobot = RKConvenienceRobot(robot: noteRobot) {
+                    if (UIApplication.shared.applicationState != .active) {
+                        conveniencerobot.disconnect()
+                    } else {
+                        robot = RKConvenienceRobot(robot: noteRobot)
+                        connectionLabel.text = noteRobot.name()
+                        responseText.text = "Greetings mortals."
+                        robot.add(self) //get responses from sphero
+                        robot.enableStabilization(true)
+                        robot.enableCollisions(true)
+                        robot.enableLocator(true)
+                        let sensorMask = RKDataStreamingMask.locatorAll
+                        robot.enableSensors(sensorMask, at: RKStreamingRate.dataStreamingRate100)
+                        robot.setZeroHeading() //current position is (0,0)
+                        robot.setLEDWithRed(0.1, green: 0.8, blue: 0.2)
+                    }
+                    break
+                }
+            case .disconnected:
+                NSLog("Sphero Disconnected")
+                connectionLabel.text = "Disconnected"
+                robot = nil;
+                // eventually will have to redraw empty grid
+                // gridView.setNeedsDisplay()
+                startDiscovery()
+                break
+            default:
+                NSLog("State change with state: \(notification)")
             }
-            break
-        case .disconnected:
-            connectionLabel.text = "Disconnected"
-            startDiscovery()
-            robot = nil;
-            
-            break
-        default:
-            NSLog("State change with state: \(notification.type)")
         }
     }
    
